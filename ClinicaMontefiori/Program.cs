ffbd4aacff6e1616fa86d9c896fd122a66ecb141
gu <dguzman109@gmail.com>
2023-04-08 16:40:48 -0500

    Frm historial Clinico

--- ClinicaMontefiori/Program.cs
+++ ClinicaMontefiori/Program.cs
@@ -16,7 +16,7 @@ namespace ClinicaMontefiori
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Recepcionista());
+            Application.Run(new HistorialClinico());
         }
     }
 }
